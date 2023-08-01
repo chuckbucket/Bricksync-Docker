97673f7b571a2905b6da556c5b806566380b120d
chuckbucket <79345699+chuckbucket@users.noreply.github.com>
2023-08-01 12:48:54 -0600

    Update Entrypoint.sh

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -10,9 +10,9 @@ echo "Hello"
 time=$(date)
 
 # Perform the necessary configuration using sed
-sed -e "s|{BRICKLINK_CONSUMER_KEY}|$BRICKLINK_CONSUMER_KEY|" \
-    -e "s|{BRICKLINK_CONSUMER_SECRET}|$BRICKLINK_CONSUMER_SECRET|" \
-    -e "s|{BRICKLINK_TOKEN}|$BRICKLINK_TOKEN|" \
-    -e "s|{BRICKLINK_TOKEN_SECRET}|$BRICKLINK_TOKEN_SECRET|" \
-    -e "s|{BRICKOWL_KEY}|$BRICKOWL_KEY|" \
-    /app/data/bricksync.conf.txt.template > /app/data/bricksync.conf.txt
+#sed -e "s|{BRICKLINK_CONSUMER_KEY}|$BRICKLINK_CONSUMER_KEY|" \
+#    -e "s|{BRICKLINK_CONSUMER_SECRET}|$BRICKLINK_CONSUMER_SECRET|" \
+#    -e "s|{BRICKLINK_TOKEN}|$BRICKLINK_TOKEN|" \
+#    -e "s|{BRICKLINK_TOKEN_SECRET}|$BRICKLINK_TOKEN_SECRET|" \
+#    -e "s|{BRICKOWL_KEY}|$BRICKOWL_KEY|" \
+#    /app/data/bricksync.conf.txt.template > /app/data/bricksync.conf.txt
