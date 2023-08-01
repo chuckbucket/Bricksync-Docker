d840618d389df349a14aabfb0fde3dfdd870b2a8
chuckbucket <79345699+chuckbucket@users.noreply.github.com>
2023-08-01 12:14:49 -0600

    Update and rename entrypoint.sh to Entrypoint.sh

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -3,15 +3,10 @@
 echo "Hello"
 time=$(date)
 
-
-
 # Perform the necessary configuration using sed
 sed -e "s|{BRICKLINK_CONSUMER_KEY}|$BRICKLINK_CONSUMER_KEY|" \
     -e "s|{BRICKLINK_CONSUMER_SECRET}|$BRICKLINK_CONSUMER_SECRET|" \
     -e "s|{BRICKLINK_TOKEN}|$BRICKLINK_TOKEN|" \
     -e "s|{BRICKLINK_TOKEN_SECRET}|$BRICKLINK_TOKEN_SECRET|" \
     -e "s|{BRICKOWL_KEY}|$BRICKOWL_KEY|" \
-    data/bricksync.conf.txt.template > data/bricksync.conf.txt
-
-# Execute the main command
-#exec ./bricksync
+    /app/data/bricksync.conf.txt.template > /app/data/bricksync.conf.txt
